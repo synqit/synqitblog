a3235ae7bb08407cdde9a8fbd350716767dabb32
Saskia <saskia@synqit.nl>
2026-01-17 14:47:18 +0100

    Configure Github Pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://synqit.github.io',
+	base: '/synqitblog-pages',
 	integrations: [mdx(), sitemap()],
 });
